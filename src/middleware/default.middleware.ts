651bcb44636b508f4d9159f59916a56570feb8df
shanalefevre <shanoulefevre@orange.fr>
2024-11-05 11:40:17 +0100

    avancement

--- src/middleware/default.middleware.ts
+++ src/middleware/default.middleware.ts
@@ -8,8 +8,26 @@ export class DefaultMiddleware {
 
     public static async exampleToDelete(req: Request, res: Response) {
         try {
-            const users = serverInstance.getPrismaClient().user.findMany();
+            const users = await serverInstance.getPrismaClient().user.findMany();
             res.status(200).send(users);
+        } catch (error) {
+            res.status(500).send(`An error occured : ${error}`);
+        }
+    }
+
+    
+    public static async getConfirmationCommande(req: Request, res: Response) {
+        try {
+            console.log(req.params['id']);
+            //récupération du nom grace à l'id
+            const name = await serverInstance.getPrismaClient().user.findUnique({
+                where: {
+                    id: parseInt(req.params['id'])
+                }
+            });
+            res.status(200).send(name);
+           
+
         } catch (error) {
             res.status(500).send(`An error occured : ${error}`);
         }
--- src/routes/default.routes.ts
+++ src/routes/default.routes.ts
@@ -4,4 +4,5 @@ import { DefaultMiddleware } from "../middleware/default.middleware";
 export default function (app: Application) {
     app.get("/", DefaultMiddleware.healthcheck);
     app.get("/example", DefaultMiddleware.exampleToDelete);
+    app.get("/getConfirmationCommande/:id", DefaultMiddleware.getConfirmationCommande);
 }
